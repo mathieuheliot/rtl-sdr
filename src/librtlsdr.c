20c779cf8d67ffb316c87fbf713691ce899ad12a
Mathieu Héliot <mathieu.heliot@gmail.com>
2020-11-11 10:19:32 +0100

    fix : Import of libusb

--- src/librtlsdr.c
+++ src/librtlsdr.c
@@ -27,7 +27,7 @@
 #define min(a, b) (((a) < (b)) ? (a) : (b))
 #endif
 
-#include <libusb.h>
+#include <libusb-1.0/libusb.h>
 
 /*
  * All libusb callback functions should be marked with the LIBUSB_CALL macro
